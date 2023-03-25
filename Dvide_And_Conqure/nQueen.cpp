2e89ea09bb5f9d2814c2ae2f68785e60f90f6375
Deepaktumula <tumuladeepak.cse.2019@nist.edu>
2023-03-25 08:32:13 +0530

    nQueenProblem

---
+++ Dvide_And_Conqure/nQueen.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printSolution(vector<vector<char>> &board, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << board[i][j] << "  ";
+        }
+        cout << endl;
+    }
+    cout << endl << endl;
+}
+
+bool isSafe(int row, int col, vector<vector<char>> &board, int n)
+{
+    // Chechk Whether Queen can be placed in cureent position or not
+    int i = row;
+    int j = col;
+    // check row
+    while (j >= 0)
+    {
+        if (board[i][j] == 'Q')
+        {
+            return false;
+        }
+        j--;
+    }
+    // check upper left diagonal
+    i = row;
+    j = col;
+    while (i >= 0 && j >= 0)
+    {
+        if (board[i][j] == 'Q')
+        {
+            return false;
+        }
+        i--;
+        j--;
+    }
+    // check bottom diagonal
+    i = row;
+    j = col;
+    while (i < n && j >= 0)
+    {
+        if (board[i][j] == 'Q')
+        {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
+
+void solve(vector<vector<char>> &board, int col, int n)
+{
+    // Base Case
+    if (col >= n)
+    {
+        printSolution(board, n);
+        return;
+    }
+
+    // Processing
+    // 1 case solve karna Baaki Recursion Sambhal lega
+    for (int row = 0; row < n; row++)
+    {
+        if (isSafe(row, col, board, n))
+        {
+            // Rakh do
+            board[row][col] = 'Q';
+
+            // Recursive Relation
+            solve(board, col + 1, n);
+
+            // backtracking
+            board[row][col] = '-';
+        }
+    }
+}
+
+int main()
+{
+    int n = 4;
+    vector<vector<char>> board(n, vector<char>(n, '-'));
+    int col = 0;
+    solve(board, col, n);
+    return 0;
+}
